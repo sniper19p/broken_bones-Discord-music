d85251d350200f99d82077061e7db24a96c0bf58
sniper19p <67769442+sniper19p@users.noreply.github.com>
2021-10-15 11:15:29 -0500

    Update .isConnected().js

--- Functions/Checks/.isConnected().js
+++ Functions/Checks/.isConnected().js
@@ -1,1 +1,23 @@
+const { Client, Intents } = require('discord.js');
+const music = require('@koenie06/discord.js-music');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_VOICE_STATES] });
 
+
+client.on('interactionCreate', async (interaction) => {
+    if(interaction.isCommand()) {
+        if(interaction.commandName === 'isconnected') {
+
+
+            const isConnected = await music.isConnected({ interaction: interaction });
+         /*
+              'true' means that it is connected to a VoiceChannel.
+              'false' means that it isn't connected to any VoiceChannel.
+            */
+            interaction.reply({ content: isConnected === true ? 'I am connected!' : 'Welp.. I am not connected to any channel here.' });
+
+        };
+    };
+});
+
+
+client.login('Token of your bot');
