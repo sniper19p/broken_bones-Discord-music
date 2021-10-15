fbc01501c5dfdb7f67ffa3f8180ab3420f973dd2
sniper19p <67769442+sniper19p@users.noreply.github.com>
2021-10-15 12:19:35 -0500

    Update events.js

--- Functions/Events/events.js
+++ Functions/Events/events.js
@@ -1,5 +1,6 @@
 
 const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_VOICE_STATES] });
 const music = require('@broken_bones/discord-music');
 const events = music.event;
 
