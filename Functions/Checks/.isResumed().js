b289a2f000b3277b2f5c77d85aa2078200e6a077
sniper19p <67769442+sniper19p@users.noreply.github.com>
2021-10-15 12:16:39 -0500

    Update .isResumed().js

--- Functions/Checks/.isResumed().js
+++ Functions/Checks/.isResumed().js
@@ -1,6 +1,6 @@
 
 const { Client, Intents } = require('discord.js');
-const music = require('@koenie06/discord.js-music');
+const music = require('@broken_bones/discord-music');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_VOICE_STATES] });
 
 
