5a73713caab11f0ee64ed07afd870b5f6dd7f263
sniper19p <67769442+sniper19p@users.noreply.github.com>
2021-10-15 11:16:26 -0500

    Create isPaused().js

---
+++ Functions/Checks/isPaused().js
@@ -0,0 +1,24 @@
+
+const { Client, Intents } = require('discord.js');
+const music = require('@koenie06/discord.js-music');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_VOICE_STATES] });
+
+
+client.on('interactionCreate', async (interaction) => {
+    if(interaction.isCommand()) {
+        if(interaction.commandName === 'ispaused') {
+
+            /*
+              'true' means that the playing song is paused.
+              'false' means that the playing song is still playing.
+            */
+
+            const isPaused = await music.isPaused({ interaction: interaction });
+    
+            interaction.reply({ content: isPaused === true ? 'I am waiting for you to resume..' : 'Yes yes, i am still playing music!' });
+
+        };
+    };
+});
+
+client.login('Token of your bot');
