e520acdf8b87a2dad77c63997d1148f6b9276c1d
sniper19p <67769442+sniper19p@users.noreply.github.com>
2021-10-17 16:38:03 -0500

    Update Jump.js

--- Functions/Music/Jump.js
+++ Functions/Music/Jump.js
@@ -2,3 +2,24 @@
 const { Client, Intents } = require('discord.js');
 const music = require('@broken_bones/discord-music');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_VOICE_STATES] });
+
+client.on('interactionCreate', async (interaction) => {
+    if(interaction.isCommand()) {
+        if(interaction.commandName === 'jump') {
+
+            /*
+             * Must be a vaild queue number
+            */
+
+            const number = interaction.options.getInteger('number');
+
+            music.jump({
+                interaction: interaction,
+                number: number
+            });
+        };
+    };
+});
+
+
+client.login('Your bots token');
