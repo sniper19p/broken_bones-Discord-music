b15912a407579eb5ffb1a0541d7b3074d98e34bc
sniper19p <67769442+sniper19p@users.noreply.github.com>
2021-10-17 16:35:33 -0500

    Update Resume.js

--- Functions/Music/Resume.js
+++ Functions/Music/Resume.js
@@ -2,3 +2,12 @@
 const { Client, Intents } = require('discord.js');
 const music = require('@broken_bones/discord-music');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_VOICE_STATES] });
+
+client.on('interactionCreate', async (interaction) => {
+    if(interaction.isCommand()) {
+        if(interaction.commandName === 'resume') music.resume({ interaction: interaction });
+    };
+});
+
+
+client.login('Your bots token');
