de4c8dbd9470f266c416c4f758412ef28b8f0bed
sniper19p <67769442+sniper19p@users.noreply.github.com>
2021-10-17 16:30:22 -0500

    Update Skip.js

--- Functions/Music/Skip.js
+++ Functions/Music/Skip.js
@@ -1,1 +1,4 @@
 
+const { Client, Intents } = require('discord.js');
+const music = require('@broken_bones/discord-music');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_VOICE_STATES] });
