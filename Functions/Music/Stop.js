18ceec2da9aeb79c20585837f4e606631c9dc18c
sniper19p <67769442+sniper19p@users.noreply.github.com>
2021-10-17 16:29:04 -0500

    Update Stop.js

--- Functions/Music/Stop.js
+++ Functions/Music/Stop.js
@@ -1,13 +1,3 @@
+
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const music = require('@broken_bones/discord-music');
-
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_VOICE_STATES] });
-
-client.on('interactionCreate', async (interaction) => {
-    if(interaction.isCommand()) {
-        if(interaction.commandName === 'stop') music.stop({ interaction: interaction });
-    };
-});
-
-
-client.login('Your bots token');
