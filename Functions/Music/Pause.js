6b9b90e281e5ab189ca28095773f88163c81a286
sniper19p <67769442+sniper19p@users.noreply.github.com>
2021-10-17 16:35:12 -0500

    Update Pause.js

--- Functions/Music/Pause.js
+++ Functions/Music/Pause.js
@@ -2,3 +2,11 @@
 const { Client, Intents } = require('discord.js');
 const music = require('@broken_bones/discord-music');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_VOICE_STATES] });
+client.on('interactionCreate', async (interaction) => {
+    if(interaction.isCommand()) {
+        if(interaction.commandName === 'pause') music.pause({ interaction: interaction });
+    };
+});
+
+
+client.login('Your bots token');
