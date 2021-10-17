b59ddae4d1e9cc4f7fcbde71c80365a7886aa653
sniper19p <67769442+sniper19p@users.noreply.github.com>
2021-10-17 16:39:07 -0500

    Update Removequeue.js

--- Functions/Music/Removequeue.js
+++ Functions/Music/Removequeue.js
@@ -2,3 +2,23 @@
 const { Client, Intents } = require('discord.js');
 const music = require('@broken_bones/discord-music');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_VOICE_STATES] });
+
+client.on('interactionCreate', async (interaction) => {
+    if(interaction.isCommand()) {
+        if(interaction.commandName === 'removequeue') {
+
+            /*
+             * Must be vaild queue number
+            */
+
+            const number = interaction.options.getInteger('number');
+
+            music.removeQueue({
+                interaction: interaction,
+                number: number 
+            });
+
+        };
+    };
+});
+client.login('Your bots token');
