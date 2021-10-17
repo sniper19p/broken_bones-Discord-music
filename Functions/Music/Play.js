bb5483343f5c50a5efe5c98058aa5c8edb2997f3
sniper19p <67769442+sniper19p@users.noreply.github.com>
2021-10-17 16:33:00 -0500

    Update Play.js

--- Functions/Music/Play.js
+++ Functions/Music/Play.js
@@ -2,3 +2,19 @@
 const { Client, Intents } = require('discord.js');
 const music = require('@broken_bones/discord-music');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_VOICE_STATES] });
+
+client.on('interactionCreate', async (interaction) => {
+    if(interaction.isCommand()) {
+        if(interaction.commandName === 'play') {
+
+           const channel = interaction.member.voice.channel;
+           const song = interaction.options.getString('song');
+
+           music.play({
+               interaction: interaction,
+               channel: channel,
+               song: song
+           });
+        };
+    };
+});
