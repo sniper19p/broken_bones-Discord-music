e68b613d7a2cd08775d537dd35a7fc4312e5ca63
sniper19p <67769442+sniper19p@users.noreply.github.com>
2021-10-17 16:37:15 -0500

    Update Volume.js

--- Functions/Music/Volume.js
+++ Functions/Music/Volume.js
@@ -2,3 +2,24 @@
 const { Client, Intents } = require('discord.js');
 const music = require('@broken_bones/discord-music');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_VOICE_STATES] });
+
+client.on('interactionCreate', async (interaction) => {
+    if(interaction.isCommand()) {
+        if(interaction.commandName === 'volume') {
+            
+            /*
+             * Can't be higher than 100
+            */
+            
+            const volume = interaction.options.getInteger('volume');
+            
+            music.volume({
+                interaction: interaction,
+                volume: volume
+            });
+        };
+    };
+});
+
+
+client.login('Your bots token');
